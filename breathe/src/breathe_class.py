1509f5243c3baabdd4a01a19da65ef19657a1ba0
angunesdogdu <angunesdogdu@aselsan.com.tr>
2021-08-31 16:03:04 +0300

    fixed 2

--- breathe/src/breathe_class.py
+++ breathe/src/breathe_class.py
@@ -15,6 +15,7 @@ import argparse
 import control_msgs.msg
 import moveit_commander
 import rospy
+import tf
 import trajectory_msgs.msg
 
 class BreathingSource(object):
@@ -45,13 +46,42 @@ class Breathe(object):
         self.scene = moveit_commander.PlanningSceneInterface()
         self.group_name = "manipulator"
         self.group = moveit_commander.MoveGroupCommander(self.group_name)
+        self.end_effector_link = self.group.get_end_effector_link()
+        self.group.allow_replanning(True)
+
+        self.group.set_goal_position_tolerance(0.01)
+        self.group.set_goal_orientation_tolerance(0.1)
+        self.group.set_planning_time(1)
+        
+        JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 
+            'elbow_joint','wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+        # Check whether simulation or not
+        if rospy.has_param('arm_controller'):
+            namespace_ = '/arm_controller/follow_joint_trajectory'
+        else:
+            namespace_ = '/follow_joint_trajectory'
+        # Create an action client
+        self.client = actionlib.SimpleActionClient(
+            namespace_, # namespace of the action topics
+            control_msgs.msg.FollowJointTrajectoryAction # action type
+        )
+        print "Waiting for server..."
+        self.client.wait_for_server()
+        print "Connected to server"
+
+        self.g = control_msgs.msg.FollowJointTrajectoryGoal()
+        self.g.trajectory = trajectory_msgs.msg.JointTrajectory()
+        self.g.trajectory.joint_names = JOINT_NAMES
+        self.gaze_point = np.array([-0.6, 1.2, 0.0])
+
+        self.send_default()
 
         self.waypoints = []
         self.start_pose = self.group.get_current_pose().pose
         self.pose_next = copy.deepcopy(self.start_pose)
         self.move_vector = (np.array([1, 1, 1])
             /np.linalg.norm(np.array([1, 1, 1])))
-        self.gaze_point = np.array([0.2, 0.0, -0.2])
+        
         self.plan = None
 
         
@@ -67,7 +97,7 @@ class Breathe(object):
         d = 0
 
         while d < 33:
-            scale = volume_data[ind%33]
+            scale = 0.005*volume_data[ind%33]
             ind += 1
             d += 1
             self.pose_next.position.z = self.start_pose.position.z + scale * self.move_vector[2]  # First move up (z)
@@ -79,6 +109,35 @@ class Breathe(object):
         self.plan_path()
         self.execute_path()
 
+    def send_default(self):
+        start_pose = self.group.get_current_pose().pose
+        wpose = self.group.get_current_pose(self.end_effector_link).pose
+        print start_pose
+        print wpose
+
+        matrixx = tf.transformations.quaternion_matrix([wpose.orientation.x,wpose.orientation.y,wpose.orientation.z,wpose.orientation.w])
+        inv_matrixx = tf.transformations.inverse_matrix(matrixx)
+        vectorx = np.dot(([1,0,0,1]),inv_matrixx)
+        print vectorx
+        wpose.position.x = -0.26
+        wpose.position.y = 0.19
+        wpose.position.z = 0.57
+        gaze_vector_W = self.gaze_point - np.array(
+            [wpose.position.x,wpose.position.y, wpose.position.z])
+        gaze_vector_W = gaze_vector_W/np.linalg.norm(gaze_vector_W)
+        axis = np.cross([1,0,0],gaze_vector_W)
+        angle = math.acos(np.dot([1,0,0],gaze_vector_W))
+        axis = axis/np.linalg.norm(axis)
+        sin_ = math.sin(angle/2)
+        wpose.orientation.x = axis[0]*sin_
+        wpose.orientation.y = axis[1]*sin_
+        wpose.orientation.z = axis[2]*sin_
+        wpose.orientation.w = math.cos(angle/2)
+        self.group.set_pose_target(wpose)
+        plan = self.group.go(wait=True)
+        #self.group.execute(plan, wait=True)
+        self.group.stop()
+        self.group.clear_pose_targets()
 
 
     def maintain_gaze(self):
@@ -86,17 +145,15 @@ class Breathe(object):
             gaze_vector_W = self.gaze_point - np.array(
                 [wpose.position.x, wpose.position.y, wpose.position.z])
             gaze_vector_W = gaze_vector_W/np.linalg.norm(gaze_vector_W) 
-            print gaze_vector_W
-            angle = np.dot(gaze_vector_W,[0,0,1])
-            print angle
-            axis = np.cross(gaze_vector_W,[0,0,1])
+            angle = math.acos(np.dot([1,0,0],gaze_vector_W))
+            axis = np.cross([1,0,0],gaze_vector_W)
             axis = axis/np.linalg.norm(axis)
-            print axis
             sin_ = math.sin(angle/2)
             wpose.orientation.x = axis[0]*sin_
             wpose.orientation.y = axis[1]*sin_
             wpose.orientation.z = axis[2]*sin_
             wpose.orientation.w = math.cos(angle/2)
+            print wpose
 
     def plan_path(self):
         self.plan, fraction = self.group.compute_cartesian_path(
@@ -105,32 +162,13 @@ class Breathe(object):
             0.0)         # jump_threshold
 
     def execute_path(self):
-        JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 
-            'elbow_joint','wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
-        # Check whether simulation or not
-        if rospy.has_param('arm_controller'):
-            namespace_ = '/arm_controller/follow_joint_trajectory'
-        else:
-            namespace_ = '/follow_joint_trajectory'
-        # Create an action client
-        client = actionlib.SimpleActionClient(
-            namespace_, # namespace of the action topics
-            control_msgs.msg.FollowJointTrajectoryAction # action type
-        )
-        print "Waiting for server..."
-        client.wait_for_server()
-        print "Connected to server"
-
-        g = control_msgs.msg.FollowJointTrajectoryGoal()
-        g.trajectory = trajectory_msgs.msg.JointTrajectory()
-        g.trajectory.joint_names = JOINT_NAMES
-        d= 0.0
+        d= 5.0
         for point in self.plan.joint_trajectory.points:
             d += 0.1 # change later
-            g.trajectory.points.append(
+            self.g.trajectory.points.append(
                 trajectory_msgs.msg.JointTrajectoryPoint(positions=point.positions,
                 velocities=[0,0,0,0,0,0], time_from_start=rospy.Duration(d)))
-        client.send_goal(g)
+        self.client.send_goal(self.g)
 
         
         
